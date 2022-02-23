ab9659d7a9c946936ceaaa73daba8bbc8184e34d
Gergo Boros <boros.gergo@novaservices.hu>
2022-02-23 19:07:36 +0100

    Create Backend Spring project

---
+++ programsch-backend/src/main/java/hu/bme/aut/programsch/ProgramSchApplication.java
@@ -0,0 +1,11 @@
+package hu.bme.aut.programsch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramSchApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ProgramSchApplication.class,args);
+    }
+}
