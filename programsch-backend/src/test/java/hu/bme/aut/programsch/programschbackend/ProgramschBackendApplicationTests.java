5ec6773b636cb9cc01bc40f96e386077d307b058
Gergo Boros <boros.gergo@novaservices.hu>
2022-02-23 18:33:42 +0100

    Add backend

---
+++ programsch-backend/src/test/java/hu/bme/aut/programsch/programschbackend/ProgramschBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.bme.aut.programsch.programschbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramschBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
